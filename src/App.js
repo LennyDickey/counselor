8a07f1ce3b04eeb1c468010d7a3437f6a084d6de
Lenny Dickey <Len.dickey@protonmail.com>
2019-11-07 15:54:13 -0500

    ready to merge

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
-import "./style/wave.css";
 
 import Home from "./components/home";
 
+import Survey from "./components/students-survey";
+import PathWay from "./components/PathWayCard";
 import Profile from "./components/profile";
+import StudentInfo from "./components/StudentInfo";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Home />
-      <div>
+
+      <div className="container">
         <Route path="/Home" exact component={Home} />
+
+        <Route path="/" exact component={Profile} />
+        <Route path="/survey" render={() => <Survey />} />
+        <Route path="/pathWay" render={() => <PathWay />} />
+        <Route path="/students/" component={StudentInfo} />
       </div>
 
       <Footer />
